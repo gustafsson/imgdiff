687e9ecf27040cabac166afea9f96b4175ba7136
Johan Gustafsson <johan.gustafsson@muchdifferent.com>
2013-06-19 12:06:39 +0200

    Build the latest release build when making the ImgDiff.app

--- makeapp.sh
+++ makeapp.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+# Build the latest Release build
+/Applications/MonoDevelop.app/Contents/MacOS/mdtool build -c:"Release|x86" ImgDiff.sln
+
+# couldn't get macpack to work
 # macpack -m:2 -o:.  -r:/Library/Frameworks/Mono.framework/Versions/Current/lib/ -n:ImgDiff -a:ImgDiff/bin/Release/ImgDiff.exe
 
+# manually creating an app instead
 rm -rf ImgDiff.app/Contents/MacOS
 rm -rf ImgDiff.app/Contents/Resources
 mkdir -p ImgDiff.app/Contents/MacOS
